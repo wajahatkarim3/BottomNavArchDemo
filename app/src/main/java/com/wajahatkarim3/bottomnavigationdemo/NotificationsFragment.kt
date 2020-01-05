5837072c85e5de4cd45140e515a91122664b95cf
Wajahat Karim <wajahatkarim3@gmail.com>
2020-01-05 19:49:20 +0500

    Added Notification Details

--- app/src/main/java/com/wajahatkarim3/bottomnavigationdemo/NotificationsFragment.kt
+++ app/src/main/java/com/wajahatkarim3/bottomnavigationdemo/NotificationsFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_notifications.*
 
 /**
  * A simple [Fragment] subclass.
@@ -14,6 +16,18 @@ class NotificationsFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_notifications, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupViews()
+    }
+
+    fun setupViews()
+    {
+        btnNotificationDetails.setOnClickListener {
+            findNavController().navigate(R.id.action_notificationsFragment_to_notificationDetailsFragment)
+        }
     }
 
 }
