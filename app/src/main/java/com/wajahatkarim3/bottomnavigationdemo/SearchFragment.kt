efa9682e6ce2dc7de1a04c7b2f8407b7a2cc04fe
Wajahat Karim <wajahatkarim3@gmail.com>
2020-01-05 19:59:53 +0500

    Added Options Menu in Search

--- app/src/main/java/com/wajahatkarim3/bottomnavigationdemo/SearchFragment.kt
+++ app/src/main/java/com/wajahatkarim3/bottomnavigationdemo/SearchFragment.kt
@@ -1,19 +1,26 @@
 package com.wajahatkarim3.bottomnavigationdemo
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 
 /**
  * A simple [Fragment] subclass.
  */
 class SearchFragment : Fragment() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_search, container, false)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.search_menu, menu)
+        true
+    }
 }
