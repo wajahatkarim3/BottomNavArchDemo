4a5e3e0331f5a38b2b3e588dfa0440a3071b2c2a
Wajahat Karim <wajahatkarim3@gmail.com>
2020-01-05 20:30:45 +0500

    Added Double Tap exit

--- app/src/main/java/com/wajahatkarim3/bottomnavigationdemo/MainActivity.kt
+++ app/src/main/java/com/wajahatkarim3/bottomnavigationdemo/MainActivity.kt
@@ -2,6 +2,10 @@ package com.wajahatkarim3.bottomnavigationdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.widget.Toast
+import androidx.core.os.postDelayed
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
@@ -11,6 +15,8 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,10 +28,34 @@ class MainActivity : AppCompatActivity() {
     fun setupViews()
     {
         var navHostFragment = supportFragmentManager.findFragmentById(R.id.fragNavHost) as NavHostFragment
+        navController = navHostFragment.navController
         NavigationUI.setupWithNavController(bottomNavView, navHostFragment.navController)
 
         //var appBarConfiguration = AppBarConfiguration(navHostFragment.navController.graph)
         var appBarConfiguration = AppBarConfiguration(setOf(R.id.homeFragment, R.id.searchFragment, R.id.notificationsFragment, R.id.profileFragment))
         setupActionBarWithNavController(navHostFragment.navController, appBarConfiguration)
+    }
+
+    private var backPressedOnce = false
+
+    override fun onBackPressed() {
+        if (navController.graph.startDestination == navController.currentDestination?.id)
+        {
+            if (backPressedOnce)
+            {
+                super.onBackPressed()
+                return
+            }
+
+            backPressedOnce = true
+            Toast.makeText(this, "Press BACK again to exit", Toast.LENGTH_SHORT).show()
+
+            Handler().postDelayed(2000) {
+                backPressedOnce = false
+            }
+        }
+        else {
+            super.onBackPressed()
+        }
     }
 }
