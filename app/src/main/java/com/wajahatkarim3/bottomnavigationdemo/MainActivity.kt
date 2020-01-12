9fe57620fdefba2200acaee2ff1f9c62d49a4600
Wajahat Karim <wajahatkarim3@gmail.com>
2020-01-13 03:00:11 +0500

    Basic BottomNavigationa and Toolbar Customizations

--- app/src/main/java/com/wajahatkarim3/bottomnavigationdemo/MainActivity.kt
+++ app/src/main/java/com/wajahatkarim3/bottomnavigationdemo/MainActivity.kt
@@ -2,6 +2,10 @@ package com.wajahatkarim3.bottomnavigationdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +13,27 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupViews()
+    }
+
+    fun setupViews()
+    {
+        // Finding the Navigation Controller
+        var navController = findNavController(R.id.fragNavHost)
+
+        // Setting Navigation Controller with the BottomNavigationView
+        bottomNavView.setupWithNavController(navController)
+
+        // Setting Up ActionBar with Navigation Controller
+        var appBarConfiguration = AppBarConfiguration(
+            topLevelDestinationIds = setOf (
+                R.id.homeFragment,
+                R.id.searchFragment,
+                R.id.notificationsFragment,
+                R.id.profileFragment
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 }
